ef50b2d67916825adf3d339c1f962a417fcefb5e
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2023-11-13 04:18:41 -0800

    update

--- _printf.c
+++ _printf.c
@@ -10,6 +10,9 @@
  */
 int printf(const char *format, ...)
 {
+	if (!format)
+		return -1;
+
 	va_list args;
 	va_start(args, format);
 
@@ -26,8 +29,17 @@ int printf(const char *format, ...)
 					count += _putchar(va_arg(args, int));
 					break;
 				case 's':
-					count += printf("%s", va_arg(args, char *));
-					break;
+					{
+						char *str = va_arg(args, char *);
+						if (!str)
+							str = "(null)";
+						while (*str != '\0')
+						{
+							count += _putchar(*str);
+							str++;
+						}
+						break;
+					}
 				case '%':
 					count += _putchar('%');
 					break;
