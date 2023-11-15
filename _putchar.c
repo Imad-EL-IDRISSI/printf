1e3e181ae9bcb4fc27e0dd5a8cd0fa556bc9015f
TachiYassine <tachi.yassine.3@gmail.com>
2023-11-14 16:33:36 -0800

    Update

--- _putchar.c
+++ _putchar.c
@@ -2,13 +2,12 @@
 #include "main.h"
 
 /**
- * _putchar -writes a character to the standard output (stdout)
+ * _putchar - writes a character to the standard output (stdout)
  * @c: the character to be written
- *
  * Return: On success, 1 is returned, -1  otherwise
  */
 
 int _putchar(char c)
 {
-	return (write(1, &c ,1));
+	return (write(1, &c, 1));
 }
